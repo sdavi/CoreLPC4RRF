1830722d365dc2e223113921c22517ffc5d441af
sdavi <stephen@Stephens-Air.fritz.box>
2018-12-13 18:50:33 +1100

    Fix setting baud rate for UART

--- CoreLPC2/cores/ExploreM3/usart.cpp
+++ CoreLPC2/cores/ExploreM3/usart.cpp
@@ -71,60 +71,168 @@ usart_dev *USART3 = &usart3;
 
 
  
-void usart_init(usart_dev *dev) {
-  uint32_t usartPclk,pclk,RegValue;
-          
+
+//Serial_baud from MBED
+void serial_baud(LPC_UART_TypeDef *obj, int baudrate) {
+
+    // set pclk to /1
+    if( obj == (LPC_UART_TypeDef *)LPC_UART0_BASE){ LPC_SC->PCLKSEL0 &= ~(0x3 <<  6); LPC_SC->PCLKSEL0 |= (0x1 <<  6);}
+    else if( obj == (LPC_UART_TypeDef *)LPC_UART1_BASE){ LPC_SC->PCLKSEL0 &= ~(0x3 <<  8); LPC_SC->PCLKSEL0 |= (0x1 <<  8); }
+    else if( obj == (LPC_UART_TypeDef *)LPC_UART2_BASE){ LPC_SC->PCLKSEL1 &= ~(0x3 << 16); LPC_SC->PCLKSEL1 |= (0x1 << 16); }
+    else if( obj == (LPC_UART_TypeDef *)LPC_UART3_BASE){ LPC_SC->PCLKSEL1 &= ~(0x3 << 18); LPC_SC->PCLKSEL1 |= (0x1 << 18); }
+    else {
+        //error("serial_baud");
+        return;
         
-        GPIO_PinFunction(dev->channel->TxPin,dev->channel->PinFunSel);
-        GPIO_PinFunction(dev->channel->RxPin,dev->channel->PinFunSel);
-		util_BitSet(LPC_SC->PCONP,dev->channel->pconBit);
-        
-        /* Enable FIFO and reset Rx/Tx FIFO buffers */
-        dev->channel->UARTx->FCR = (1<<SBIT_FIFO) | (1<<SBIT_RxFIFO) | (1<<SBIT_TxFIFO); 
-
-        /* 8bit data, 1Stop bit, No parity */
-        dev->channel->UARTx->LCR = (0x03<<SBIT_WordLenght) | (1<<SBIT_DLAB);
-
-        /** Baud Rate Calculation :
-       PCLKSELx registers contains the PCLK info for all the clock dependent peripherals.
-       Bit6,Bit7 contains the Uart Clock(ie.UART_PCLK) information.
-       The UART_PCLK and the actual Peripheral Clock(PCLK) is calculated as below.
-       (Refer data sheet for more info)
-
-       UART_PCLK    PCLK
-         0x00       SystemFreq/4        
-         0x01       SystemFreq
-         0x02       SystemFreq/2
-         0x03       SystemFreq/8   
-         **/
-
-        usartPclk = (LPC_SC->PCLKSEL0 >> 6) & 0x03;
-
-        switch( usartPclk )
+    }
+    
+    if(baudrate == 0) return;
+    
+    
+    uint32_t PCLK = SystemCoreClock;
+    
+    // First we check to see if the basic divide with no DivAddVal/MulVal
+    // ratio gives us an integer result. If it does, we set DivAddVal = 0,
+    // MulVal = 1. Otherwise, we search the valid ratio value range to find
+    // the closest match. This could be more elegant, using search methods
+    // and/or lookup tables, but the brute force method is not that much
+    // slower, and is more maintainable.
+    uint16_t DL = PCLK / (16 * baudrate);
+    
+    uint8_t DivAddVal = 0;
+    uint8_t MulVal = 1;
+    int hit = 0;
+    uint16_t dlv;
+    uint8_t mv, dav;
+    if ((PCLK % (16 * baudrate)) != 0) {     // Checking for zero remainder
+        int err_best = baudrate, b;
+        for (mv = 1; mv < 16 && !hit; mv++)
         {
-        case 0x00:
-            pclk = SystemCoreClock/4;
-            break;
-        case 0x01:
-            pclk = SystemCoreClock;
-            break; 
-        case 0x02:
-            pclk = SystemCoreClock/2;
-            break; 
-        case 0x03:
-            pclk = SystemCoreClock/8;
-            break;
-        }    
-
-         RegValue = ( pclk / (16 * dev->baud_rate )); 
-
-		 dev->channel->UARTx->DLL = util_ExtractByte0to8(RegValue);
-         dev->channel->UARTx->DLM = util_ExtractByte8to16(RegValue);
-         util_BitClear(dev->channel->UARTx->LCR, SBIT_DLAB); // Clear DLAB after setting DLL,DLM       
-         dev->channel->UARTx->IER = 0x01; // Enable Rx  interrupt     
-         NVIC_EnableIRQ(dev->irq_NUM);          
+            for (dav = 0; dav < mv; dav++)
+            {
+                // baudrate = PCLK / (16 * dlv * (1 + (DivAdd / Mul))
+                // solving for dlv, we get dlv = mul * PCLK / (16 * baudrate * (divadd + mul))
+                // mul has 4 bits, PCLK has 27 so we have 1 bit headroom which can be used for rounding
+                // for many values of mul and PCLK we have 2 or more bits of headroom which can be used to improve precision
+                // note: X / 32 doesn't round correctly. Instead, we use ((X / 16) + 1) / 2 for correct rounding
+                
+                if ((mv * PCLK * 2) & 0x80000000) // 1 bit headroom
+                    dlv = ((((2 * mv * PCLK) / (baudrate * (dav + mv))) / 16) + 1) / 2;
+                else // 2 bits headroom, use more precision
+                    dlv = ((((4 * mv * PCLK) / (baudrate * (dav + mv))) / 32) + 1) / 2;
+                
+                // datasheet says if DLL==DLM==0, then 1 is used instead since divide by zero is ungood
+                if (dlv == 0)
+                    dlv = 1;
+                
+                // datasheet says if dav > 0 then DL must be >= 2
+                if ((dav > 0) && (dlv < 2))
+                    dlv = 2;
+                
+                // integer rearrangement of the baudrate equation (with rounding)
+                b = ((PCLK * mv / (dlv * (dav + mv) * 8)) + 1) / 2;
+                
+                // check to see how we went
+                b = abs(b - baudrate);
+                if (b < err_best)
+                {
+                    err_best  = b;
+                    
+                    DL        = dlv;
+                    MulVal    = mv;
+                    DivAddVal = dav;
+                    
+                    if (b == baudrate)
+                    {
+                        hit = 1;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    
+    // set LCR[DLAB] to enable writing to divider registers
+    obj->LCR |= (1 << 7);
+    
+    // set divider values
+    obj->DLM = (DL >> 8) & 0xFF;
+    obj->DLL = (DL >> 0) & 0xFF;
+    obj->FDR = (uint32_t) DivAddVal << 0
+    | (uint32_t) MulVal    << 4;
+    
+    // clear LCR[DLAB]
+    obj->LCR &= ~(1 << 7);
 }
 
+typedef enum {
+    ParityNone = 0,
+    ParityOdd = 1,
+    ParityEven = 2,
+    ParityForced1 = 3,
+    ParityForced0 = 4
+} SerialParity;
+
+void serial_format(LPC_UART_TypeDef *obj, int data_bits, SerialParity parity, int stop_bits) {
+    // 5 data bits = 0 ... 8 data bits = 3
+    if (data_bits < 5 || data_bits > 8) {
+        //error("Invalid number of bits (%d) in serial format, should be 5..8", data_bits);
+        return;
+    }
+
+    data_bits -= 5;
+
+    int parity_enable, parity_select;
+    switch (parity) {
+        case ParityNone: parity_enable = 0; parity_select = 0; break;
+        case ParityOdd : parity_enable = 1; parity_select = 0; break;
+        case ParityEven: parity_enable = 1; parity_select = 1; break;
+        case ParityForced1: parity_enable = 1; parity_select = 2; break;
+        case ParityForced0: parity_enable = 1; parity_select = 3; break;
+        default:
+            //error("Invalid serial parity setting");
+            return;
+    }
+
+    // 1 stop bits = 0, 2 stop bits = 1
+    if (stop_bits != 1 && stop_bits != 2) {
+        //error("Invalid stop bits specified");
+        return;
+    }
+    stop_bits -= 1;
+
+    int break_transmission   = 0; // 0 = Disable, 1 = Enable
+    int divisor_latch_access = 0; // 0 = Disable, 1 = Enable
+    obj->LCR = data_bits << 0
+    | stop_bits << 2
+    | parity_enable << 3
+    | parity_select << 4
+    | break_transmission << 6
+    | divisor_latch_access << 7;
+}
+
+
+
+void usart_init(usart_dev *dev) {
+    uint32_t usartPclk,pclk,RegValue;
+    
+    
+    GPIO_PinFunction(dev->channel->TxPin,dev->channel->PinFunSel);
+    GPIO_PinFunction(dev->channel->RxPin,dev->channel->PinFunSel);
+    util_BitSet(LPC_SC->PCONP,dev->channel->pconBit);
+    
+    /* Enable FIFO and reset Rx/Tx FIFO buffers */
+    dev->channel->UARTx->FCR = (1<<SBIT_FIFO) | (1<<SBIT_RxFIFO) | (1<<SBIT_TxFIFO);
+    
+
+    //SD: updated use mbed function to set baud + format
+    serial_baud(dev->channel->UARTx, dev->baud_rate);
+    serial_format(dev->channel->UARTx,8, ParityNone, 1);
+    
+    
+    dev->channel->UARTx->IER = 0x01; // Enable Rx  interrupt
+    NVIC_EnableIRQ(dev->irq_NUM);
+}
 
 
 
