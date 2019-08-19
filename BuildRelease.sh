a9c2c826f421723f82065ae16da1bfbc4b93a5b2
sdavi <sdavi@users.noreply.github.com>
2019-08-19 19:47:25 +1000

    Network Updates

--- BuildRelease.sh
+++ BuildRelease.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-#make distclean
-#make firmware BUILD=Release MBED=false NETWORKING=false OUTPUT_NAME=firmware USE_DFU=false
 make distclean
 make firmware BUILD=Release MBED=false NETWORKING=true OUTPUT_NAME=firmware USE_DFU=false
 
